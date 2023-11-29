5242216580a59f5e33ab71f4f2f57f559bd70a45
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-11-29 23:16:09 +0100

    Add the neo4j-driver in order to communicate with the db

--- resolvers.js
+++ resolvers.js
@@ -1,16 +1,19 @@
+const neo4j = require('neo4j-driver');
+
+// Create a Neo4j driver instance
+const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('your-username', 'your-password'));
+const session = driver.session();
+
 const resolvers = {
     Query: {
-
     },
     Mutation: {
 
     },
     Subscription: {
         ingredientAdded: {
-
         },
     },
 };
 
 module.exports = resolvers;
-
